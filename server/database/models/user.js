b5f3e1729f2955d60be4bc474b0eeb08b2ce47d1
Jake Loew <jakeloew1@gmail.com>
2019-11-18 17:26:14 -0600

    FEATURE: sets and salts passwords

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -17,18 +17,18 @@ const User = db.define(
 		salt: {
 			type: Sequelize.STRING
 		}
+	},
+	{
+		hooks: {
+			beforeCreate: setSaltAndPassword,
+			beforeUpdate: setSaltAndPassword
+		}
 	}
-	// {
-	// 	hooks: {
-	// 		beforeCreate: setSaltAndPassword,
-	// 		beforeUpdate: setSaltAndPassword
-	// 	}
-	// }
 );
 
 // instance methods
 User.prototype.hasCorrectPassword = function(candidatePassword) {
-	if (candidatePassword === this.password) return true;
+	if (encryptPassword(candidatePassword, this.salt) === this.password) return true;
 	else return false;
 };
 
@@ -36,25 +36,21 @@ User.prototype.sanitize = function() {
 	return _.omit(this.toJSON(), [ 'password', 'salt' ]);
 };
 
-// // class methods
-// User.generateSalt = function() {
-// 	return crypto.randomBytes(16).toString('base64');
-// };
+// utility functions
+function generateSalt() {
+	return crypto.randomBytes(16).toString('base64');
+}
 
-// User.encryptPassword = function(plainText, salt) {
-// 	const hash = crypto.createHash('sha1');
-// 	hash.update(plainText);
-// 	hash.update(salt);
-// 	return hash.digest('hex');
-// };
+function encryptPassword(plainText, salt) {
+	const hash = crypto.createHash('sha1');
+	hash.update(plainText);
+	hash.update(salt);
+	return hash.digest('hex');
+}
 
-// function setSaltAndPassword(user) {
-// 	// we need to salt and hash again when the user enters their password for the first time
-// 	// and do it again whenever they change it
-// 	if (user.changed('password')) {
-// 		user.salt = User.generateSalt();
-// 		user.password = User.encryptPassword(user.password, user.salt);
-// 	}
-// }
+function setSaltAndPassword(user) {
+	user.salt = generateSalt();
+	user.password = encryptPassword(user.password, user.salt);
+}
 
 module.exports = User;
